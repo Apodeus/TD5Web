2b99e719bfa0bf6593cf62360b161f6b9048c9dc
echoffee <Echoffee@users.noreply.github.com>
2018-10-26 10:55:24 +0200

    Proper JSON -> User mapping

--- src/main/java/fr/ub/m2gl/User.java
+++ src/main/java/fr/ub/m2gl/User.java
@@ -5,9 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
-	private ObjectId _id;
 
 
     @JsonProperty("NamePerso")
@@ -17,19 +16,11 @@ public class User {
     private String lastName;
 
     public User(){
-        _id = ObjectId.get();
-    }
-
-    public User(ObjectId id, String firstName, String lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this._id = id;
     }
 
     public User(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        this._id = ObjectId.get();
     }
 
     public String getFirstName() {
@@ -47,15 +38,5 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public ObjectId get_id() {
-        return _id;
-    }
-
-    public void set_id(ObjectId _id) {
-        this._id = _id;
-    }
-
-
 
 }
