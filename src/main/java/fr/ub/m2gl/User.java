e6625eba090bc3b03e9eb8f037c9ca52f77e4775
Apodeus <ordonez.romain@gmail.com>
2018-11-02 16:39:06 +0100

    Add delete method with ID Add constructor to User to map correctly the JSON to User with the ID Fix the list all users method

--- src/main/java/fr/ub/m2gl/User.java
+++ src/main/java/fr/ub/m2gl/User.java
@@ -1,30 +1,16 @@
 package fr.ub.m2gl;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
-
-    public String get_id() {
-        return _id;
-    }
-
-    public void set_id(String _id) {
-        this._id = _id;
-    }
-
     private String _id;
 
-    //@JsonProperty("NamePerso")
     private String firstName;
 
-    //@JsonProperty("NamePerso2")
     private String lastName;
 
     public User(){
@@ -35,6 +21,12 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         _id = UUID.randomUUID().toString();
+    }
+
+    public User(String _id, String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this._id = _id;
     }
 
     public String getFirstName() {
@@ -53,4 +45,11 @@ public class User {
         this.lastName = lastName;
     }
 
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
 }
--- src/main/java/fr/ub/m2gl/UserResource.java
+++ src/main/java/fr/ub/m2gl/UserResource.java
@@ -6,22 +6,14 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import org.bson.BsonDocument;
-import org.bson.BsonReader;
-import org.bson.BsonWriter;
 import org.bson.Document;
-import org.bson.codecs.Codec;
-import org.bson.codecs.DecoderContext;
-import org.bson.codecs.EncoderContext;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.conversions.Bson;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mongodb.client.model.Projections.excludeId;
+import static java.text.MessageFormat.format;
 
 @Path("/user")
 public class UserResource {
@@ -30,55 +22,37 @@ public class UserResource {
     public static final String DB_COLLECTION = "myCollection";
     private final ObjectMapper mapper;
 
-    public UserResource(){
+    public UserResource() {
         this.mapper = new ObjectMapper();
     }
 
-
-    /*@GET
-    @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    public User convertUserToJSON(){
-        return new User("Adrien", "Halnaut");
-    }
-
-    @POST
-    @Path("/post")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response convertJSONToUser(User user){
-        String result = "User saved : " + user;
-        return Response.status(201).entity(result).build();
-    }*/
-
     @GET
     @Path("/init")
-    public String init(){
-        User usr1 = new User("Adrien", "Halnaut");
-        User usr2 = new User("Romain", "Ordonez");
-        addUserToBDD(usr1);
-        addUserToBDD(usr2);
+    public String init() {
+        add(new User("Adrien", "Halnaut"));
+        add(new User("Romain", "Ordonez"));
         return "Yay !";
     }
 
     @GET
-    @Path("/get")
+    @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         try (MongoClient mongoClient = new MongoClient()) {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(DB_COLLECTION);
 
-            FindIterable<Document> documents = collection.find().projection(excludeId());
+            FindIterable<Document> documents = collection.find();
             List<User> allUsers = new ArrayList<>();
-            for(Document doc : documents){
+            for (Document doc : documents) {
                 System.out.println(doc.toJson());
                 User user = mapper.readValue(doc.toJson(), User.class);
                 allUsers.add(user);
             }
 
             return allUsers;
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error : trying to add invalid user.", e);
         }
     }
@@ -86,42 +60,65 @@ public class UserResource {
     @GET
     @Path("/getUser")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getUser(@QueryParam("firstName") @DefaultValue("") String fname, @QueryParam("lastName") @DefaultValue("") String lname, @QueryParam("_id") @DefaultValue("") String id){
+    public List<User> getUser(@QueryParam("firstName") @DefaultValue("") String fname, @QueryParam("lastName") @DefaultValue("") String lname, @QueryParam("_id") @DefaultValue("") String id) {
         try (MongoClient mongoClient = new MongoClient()) {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(DB_COLLECTION);
             FindIterable<Document> documents = collection.find(Filters.eq("firstName", fname));
             List<User> allUsers = new ArrayList<>();
-            for(Document doc : documents){
+            for (Document doc : documents) {
                 System.out.println(doc.toJson());
                 User user = mapper.readValue(doc.toJson(), User.class);
                 allUsers.add(user);
             }
 
             return allUsers;
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error : trying to add invalid user.", e);
         }
     }
 
+    @GET
+    @Path("/delete")
+    public String getUser(@QueryParam("_id") @DefaultValue("") String id) {
+        try (MongoClient mongoClient = new MongoClient()) {
+
+            MongoDatabase database = mongoClient.getDatabase(DB_NAME);
+            MongoCollection<Document> collection = database.getCollection(DB_COLLECTION);
+            FindIterable<Document> documents = collection.find(Filters.eq("_id", id));
+            Document userDocument = null;
+            User user = null;
+            for (Document doc : documents) {
+                user = mapper.readValue(doc.toJson(), User.class);
+                userDocument = doc;
+            }
+
+            if(user == null){
+                return "There is no user in database with this id : " + id;
+            }
+
+            String firstname = user.getFirstName();
+            String lastname = user.getLastName();
+
+            collection.deleteOne(userDocument);
+
+            return format("User {0} {1} has been removed from database !", firstname, lastname);
+        } catch (Exception e) {
+            throw new RuntimeException("Error : trying to add invalid user.", e);
+        }
+    }
+
+    /**
+     * Save an User in MongoDB
+     * @param user
+     * @return a message to display
+     */
     @POST
     @Path("/add")
-    //@Consumes(MediaType.APPLICATION_JSON)
-    public String addUser(@FormParam("fname") String fname, @FormParam("lname") String lname){
-        User u = new User(fname, lname);
-        return addUserToBDD(u);
-    }
-
-    @POST
-    @Path("/addp")
     @Consumes(MediaType.APPLICATION_JSON)
-    public String addUserJson(User user){
-        return addUserToBDD(user);
-    }
-
-    private String addUserToBDD(User user) {
-        try(MongoClient mongoClient = new MongoClient()) {
+    public String add(User user) {
+        try (MongoClient mongoClient = new MongoClient()) {
             MongoDatabase db = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = db.getCollection(DB_COLLECTION);
             String jsonString = mapper.writeValueAsString(user);
@@ -133,4 +130,13 @@ public class UserResource {
         }
     }
 
+    /*
+    @POST
+    @Path("/add")
+    //@Consumes(MediaType.APPLICATION_JSON)
+    public String addUser(@FormParam("fname") String fname, @FormParam("lname") String lname){
+        User u = new User(fname, lname);
+        return saveUserToDB(u);
+    }
+    */
 }
