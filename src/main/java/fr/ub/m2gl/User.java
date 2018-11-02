28a86966cfc84d00dc49733e2de7833ec95add8c
echoffee <Echoffee@users.noreply.github.com>
2018-11-02 16:04:16 +0100

    Add fname search as a GET request

--- src/main/java/fr/ub/m2gl/User.java
+++ src/main/java/fr/ub/m2gl/User.java
@@ -5,22 +5,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import java.util.UUID;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
 
-    @JsonProperty("NamePerso")
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    private String _id;
+
+    //@JsonProperty("NamePerso")
     private String firstName;
 
-    @JsonProperty("NamePerso2")
+    //@JsonProperty("NamePerso2")
     private String lastName;
 
     public User(){
+        _id = UUID.randomUUID().toString();
     }
 
     public User(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
+        _id = UUID.randomUUID().toString();
     }
 
     public String getFirstName() {
--- src/main/java/fr/ub/m2gl/UserResource.java
+++ src/main/java/fr/ub/m2gl/UserResource.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.BsonDocument;
 import org.bson.BsonReader;
 import org.bson.BsonWriter;
@@ -67,10 +68,33 @@ public class UserResource {
 
             MongoDatabase database = mongoClient.getDatabase(DB_NAME);
             MongoCollection<Document> collection = database.getCollection(DB_COLLECTION);
+
             FindIterable<Document> documents = collection.find().projection(excludeId());
             List<User> allUsers = new ArrayList<>();
             for(Document doc : documents){
-            	System.out.println(doc.toJson());
+                System.out.println(doc.toJson());
+                User user = mapper.readValue(doc.toJson(), User.class);
+                allUsers.add(user);
+            }
+
+            return allUsers;
+        } catch(Exception e){
+            throw new RuntimeException("Error : trying to add invalid user.", e);
+        }
+    }
+
+    @GET
+    @Path("/getUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUser(@QueryParam("fname") @DefaultValue("") String fname, @QueryParam("lastName") @DefaultValue("") String lname, @QueryParam("_id") @DefaultValue("") String id){
+        try (MongoClient mongoClient = new MongoClient()) {
+
+            MongoDatabase database = mongoClient.getDatabase(DB_NAME);
+            MongoCollection<Document> collection = database.getCollection(DB_COLLECTION);
+            FindIterable<Document> documents = collection.find(Filters.eq("firstName", fname));
+            List<User> allUsers = new ArrayList<>();
+            for(Document doc : documents){
+                System.out.println(doc.toJson());
                 User user = mapper.readValue(doc.toJson(), User.class);
                 allUsers.add(user);
             }
@@ -89,6 +113,13 @@ public class UserResource {
         return addUserToBDD(u);
     }
 
+    @POST
+    @Path("/addp")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String addUserJson(User user){
+        return addUserToBDD(user);
+    }
+
     private String addUserToBDD(User user) {
         try(MongoClient mongoClient = new MongoClient()) {
             MongoDatabase db = mongoClient.getDatabase(DB_NAME);
@@ -96,7 +127,6 @@ public class UserResource {
             String jsonString = mapper.writeValueAsString(user);
             Document doc = Document.parse(jsonString);
             collection.insertOne(doc);
-
             return "Utilisateur " + user.getFirstName() + " " + user.getLastName() + " added successfully.";
         } catch (Exception e) {
             throw new RuntimeException("Error : trying to add invalid user.");
